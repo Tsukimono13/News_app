20f36ebed33ad1bbfddd4e8073752e8b759293a6
Irina Litvinova <neforsha@yandex.ru>
2023-08-10 17:16:24 +0300

    fix: refactoring

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+
+import {registerRootComponent} from 'expo';
 import { Navigation } from './components/Navigation';
 
 export default function App() {
 	return <Navigation />;
 }
 
-
+registerRootComponent(App);
